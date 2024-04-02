07c1a4b0ff16dc055162e9fae19b5f898f5b8f68
Matthew Kalinin <matthew44@mappable.world>
2024-04-02 15:33:15 +0300

    show pin with icon

--- src/MMapDefaultMarker/index.ts
+++ src/MMapDefaultMarker/index.ts
@@ -1,5 +1,6 @@
 import {MMapMarker, MMapMarkerProps} from '@mappable-world/mappable-types';
-import {IconName} from '../icons/types/icons';
+import {IconName} from '../icons/icon-name';
+import {icons} from '../icons';
 import pin from './pin.svg';
 import './index.css';
 
@@ -11,6 +12,8 @@ export type MMapDefaultMarkerProps = MMapMarkerProps & {
 export class MMapDefaultMarker extends mappable.MMapComplexEntity<MMapDefaultMarkerProps> {
     private _marker: MMapMarker;
     private _markerElement: HTMLElement;
+    private _pin: HTMLElement;
+    private _icon: HTMLElement;
 
     constructor(props: MMapDefaultMarkerProps) {
         super(props);
@@ -18,10 +21,21 @@ export class MMapDefaultMarker extends mappable.MMapComplexEntity<MMapDefaultMar
 
     protected _onAttach(): void {
         this._markerElement = document.createElement('mappable');
+        this._pin = document.createElement('mappable');
+        this._icon = document.createElement('mappable');
+
         this._markerElement.classList.add('mappable--point');
-        this._markerElement.innerHTML = pin;
         this._markerElement.style.color = this._props.color;
         this._markerElement.style.backgroundColor = this._props.color;
+
+        this._pin.classList.add('mappable--pin');
+        this._pin.innerHTML = pin;
+
+        this._icon.classList.add('mappable--icon');
+        this._icon.innerHTML = icons[this._props.name].normal;
+
+        this._pin.appendChild(this._icon);
+        this._markerElement.appendChild(this._pin);
 
         this._marker = new mappable.MMapMarker(this._props, this._markerElement);
         this.addChild(this._marker);
@@ -30,6 +44,10 @@ export class MMapDefaultMarker extends mappable.MMapComplexEntity<MMapDefaultMar
     protected _onUpdate(propsDiff: Partial<MMapDefaultMarkerProps>): void {
         if (propsDiff.color !== undefined) {
             this._markerElement.style.color = this._props.color;
+            this._markerElement.style.backgroundColor = this._props.color;
+        }
+        if (propsDiff.name !== undefined) {
+            this._icon.innerHTML = icons[propsDiff.name].normal;
         }
         this._marker.update(this._props);
     }
