4ca5d98d3c61bcadef44a2492b621e839647bbe1
Matthew Kalinin <matthew44@mappable.world>
2024-04-03 16:21:11 +0300

    color props

--- src/MMapDefaultMarker/index.ts
+++ src/MMapDefaultMarker/index.ts
@@ -1,12 +1,11 @@
 import {MMapMarker, MMapMarkerProps} from '@mappable-world/mappable-types';
-import {IconName} from '../icons/icon-name';
-import {icons} from '../icons';
-import pin from './pin.svg';
+import {IconColor, IconName, iconColors, icons, glyphColors} from '../icons';
 import './index.css';
+import pin from './pin.svg';
 
 export type MMapDefaultMarkerProps = MMapMarkerProps & {
     name: IconName;
-    color: string;
+    color: IconColor;
 };
 
 export class MMapDefaultMarker extends mappable.MMapComplexEntity<MMapDefaultMarkerProps> {
@@ -14,19 +13,19 @@ export class MMapDefaultMarker extends mappable.MMapComplexEntity<MMapDefaultMar
     private _markerElement: HTMLElement;
     private _pin: HTMLElement;
     private _icon: HTMLElement;
+    private _color: {day: string; night: string};
 
     constructor(props: MMapDefaultMarkerProps) {
         super(props);
     }
 
     protected _onAttach(): void {
+        this._color = iconColors[this._props.color];
         this._markerElement = document.createElement('mappable');
         this._pin = document.createElement('mappable');
         this._icon = document.createElement('mappable');
 
         this._markerElement.classList.add('mappable--point');
-        this._markerElement.style.color = this._props.color;
-        this._markerElement.style.backgroundColor = this._props.color;
 
         this._pin.classList.add('mappable--pin');
         this._pin.innerHTML = pin;
@@ -36,19 +35,27 @@ export class MMapDefaultMarker extends mappable.MMapComplexEntity<MMapDefaultMar
 
         this._pin.appendChild(this._icon);
         this._markerElement.appendChild(this._pin);
-
         this._marker = new mappable.MMapMarker(this._props, this._markerElement);
         this.addChild(this._marker);
+
+        this._updateTheme();
     }
 
     protected _onUpdate(propsDiff: Partial<MMapDefaultMarkerProps>): void {
         if (propsDiff.color !== undefined) {
-            this._markerElement.style.color = this._props.color;
-            this._markerElement.style.backgroundColor = this._props.color;
+            this._color = iconColors[this._props.color];
+            this._updateTheme();
         }
         if (propsDiff.name !== undefined) {
             this._icon.innerHTML = icons[propsDiff.name].normal;
         }
         this._marker.update(this._props);
+    }
+
+    private _updateTheme() {
+        this._markerElement.style.color = this._color.day;
+        this._markerElement.style.backgroundColor = this._color.day;
+        this._markerElement.style.borderColor = '#f8f8f8';
+        this._icon.style.color = glyphColors.day;
     }
 }
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,3 @@
-export {IconColor, iconColors} from './icon-colors';
+export {IconColor, iconColors, glyphColors} from './icon-colors';
 export {IconName} from './icon-name';
 export {icons} from './icons';
