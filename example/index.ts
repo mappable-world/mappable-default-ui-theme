e64946dae47d831ed52cc1b623cdbadd90ea1d87
matthew44-mappable <155086725+matthew44-mappable@users.noreply.github.com>
2024-04-04 11:29:28 +0300

    Refactoring examples and changing the structure of the main page (#3)
    * convert example from vanilla to typescript
    
    * fix
    
    * fix imports
    
    * fix test
    
    * add case template

--- example/index.ts
+++ example/index.ts
@@ -8,15 +8,6 @@ mappable.import.loaders.unshift(async (pkg) => {
     } else {
         await mappable.import.script(`https://unpkg.com/${pkg}/dist/index.js`);
     }
-
+    // @ts-ignore
     return window['@mappable-world/mappable-default-ui-theme'];
-})
-
-
-const BOUNDS = [
-    [54.58311, 25.99850],
-    [56.30248, 24.47889]
-];
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const LOCATION = {bounds: BOUNDS};
+});
