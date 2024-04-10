ca56943e5314d64d02d5ebb18d9d5ba7ccdc7d03
Matthew Kalinin <matthew44@mappable.world>
2024-04-10 17:36:57 +0300

    fix common ts case

--- example/rotate-tilt-controls/common.ts
+++ example/rotate-tilt-controls/common.ts
@@ -1,19 +1,5 @@
 import {BehaviorType, LngLatBounds, MMapBoundsLocation} from '@mappable-world/mappable-types';
 
-mappable.import.loaders.unshift(async (pkg) => {
-    if (!pkg.startsWith('@mappable-world/mappable-default-ui-theme')) {
-        return;
-    }
-
-    if (location.href.includes('localhost')) {
-        await mappable.import.script(`/dist/index.js`);
-    } else {
-        await mappable.import.script(`https://unpkg.com/${pkg}/dist/index.js`);
-    }
-    // @ts-ignore
-    return window['@mappable-world/mappable-default-ui-theme'];
-});
-
 const BOUNDS: LngLatBounds = [
     [54.58311, 25.9985],
     [56.30248, 24.47889]
