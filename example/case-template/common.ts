4f9be7b909021a27bad8a8125571a5ead45ce0b8
Matthew Kalinin <matthew44@mappable.world>
2024-06-05 13:10:34 +0300

    fix case template

--- example/case-template/common.ts
+++ example/case-template/common.ts
@@ -1,8 +1,9 @@
-import type {MMapLocationRequest, LngLatBounds} from '@mappable-world/mappable-types';
+import type {LngLat, LngLatBounds, MMapLocationRequest} from '@mappable-world/mappable-types';
 
 const BOUNDS: LngLatBounds = [
     [54.58311, 25.9985],
     [56.30248, 24.47889]
 ];
+export const CENTER: LngLat = [55.44279, 25.24107];
 
 export const LOCATION: MMapLocationRequest = {bounds: BOUNDS};
