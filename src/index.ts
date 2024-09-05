c9ca26fa93b185036d8dec165e62d0bca0448f20
Matthew Kalinin <matthew44@mappable.world>
2024-09-05 13:13:13 +0300

    add i18n for labels

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './i18n';
+
 export * from './controls';
 export * from './markers';
 export * from './ruler';
--- src/ruler/utils.ts
+++ src/ruler/utils.ts
@@ -1,11 +1,15 @@
+import i18next from 'i18next';
+
 export function formatDistance(distance: number): string {
-    return distance > 900 ? `${roundDistance(distance / 1000)} km` : `${roundDistance(distance)} m`;
+    return distance > 900
+        ? i18next.t('distance.kilometers', {value: roundDistance(distance / 1000)})
+        : i18next.t('distance.meters', {value: roundDistance(distance)});
 }
 
 export function formatArea(area: number): string {
     return area > 900_000
-        ? `${splitNumber(roundDistance(area / 1_000_000))} km²`
-        : `${splitNumber(roundDistance(area))} m²`;
+        ? i18next.t('area.squareKilometers', {value: roundDistance(area / 1_000_000)})
+        : i18next.t('area.squareMeters', {value: roundDistance(area)});
 }
 
 function roundDistance(distance: number): number {
@@ -14,8 +18,4 @@ function roundDistance(distance: number): number {
     }
     const factor = Math.pow(10, distance > 10 ? 1 : 2);
     return Math.round(distance * factor) / factor;
-}
-
-function splitNumber(value: number): string {
-    return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ');
 }
