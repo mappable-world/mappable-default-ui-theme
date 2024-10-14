908a50a9c70eda8fc306d772cc44529b1719701d
Matthew Kalinin <matthew44@mappable.world>
2024-10-14 17:01:56 +0300

    Fix ESM build for package

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
+const path = require('path');
+
+const ESM_BUILD = process.env.ESM_BUILD === 'true';
+
 module.exports = (args, env, dir = process.cwd()) => {
-    return require('@mappable-world/mappable-cli/webpack.config')(args, env, dir);
-}
+    const coreWebpackModule = require('@mappable-world/mappable-cli/webpack.config')(args, env, dir);
+    if (ESM_BUILD) {
+        coreWebpackModule.experiments = {
+            outputModule: true
+        };
+        coreWebpackModule.entry = {
+            index: {
+                import: './src/index.ts',
+                library: {
+                    type: 'module'
+                }
+            }
+        };
+        coreWebpackModule.output.path = path.resolve(dir, 'dist/esm');
+    }
+    return coreWebpackModule;
+};
