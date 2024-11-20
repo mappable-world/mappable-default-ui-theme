46ba3435b3cdd578d1ea8ac73c6798770e82d177
Matthew Kalinin <matthew44@mappable.world>
2024-11-20 16:42:33 +0300

    fix reset value

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,7 +1,14 @@
+import type {LngLat} from '@mappable-world/mappable-types';
+
 export function createMMapElement(className?: string): HTMLElement {
     const el = document.createElement('mappable');
     if (className) {
         el.className = className;
     }
     return el;
+}
+
+export function areFuzzyEqual(a: LngLat, b: LngLat, tolerance = 1e-6): boolean {
+    const d = [a[0] - b[0], a[1] - b[1]];
+    return -tolerance < d[0] && d[0] < tolerance && -tolerance < d[1] && d[1] < tolerance;
 }
